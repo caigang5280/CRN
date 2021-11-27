5fa495d178f1740e59a4966a4b9ccaaf3ca45fd4
@caigang5280 <2241340471@qq.com>
2021-11-27 18:39:05 +0800

    第一次提交git

---
+++ src/main/java/Test1.java
@@ -0,0 +1,11 @@
+/**
+ * @Version 1.0
+ * @Description TODO
+ * @Author 22413
+ * @Aate 2021/11/27 18:38
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Test111 测试");
+    }
+}
